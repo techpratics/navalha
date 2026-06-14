45d87828045f48a0e0cf1bcf337aa5f766049dbf
Arthur Lelis Uchoa <arthurlelisuchoa32@gmail.com>
2026-06-14 12:39:58 -0300

    Atualizar o AgendamentoRepository.java

--- Backend/src/main/java/com/alabamabarbers/Backend/repository/AgendamentoRepository.java
+++ Backend/src/main/java/com/alabamabarbers/Backend/repository/AgendamentoRepository.java
@@ -4,6 +4,7 @@ import com.alabamabarbers.Backend.model.Agendamento;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import com.alabamabarbers.Backend.model.StatusAgendamento;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
